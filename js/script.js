497d00133571118d82974048f9a7a9c8e6e7b886
swagking0 <bhargav.mohith@gmail.com>
2019-05-20 03:36:55 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//Get the modal
+var modal = window.document.getElementById('id01');
+
+// When the user clicks anywhere outside the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+$(window.document).ready(function() {
+    $('#sidebar').mCustomScrollbar({
+        theme: "minimal"
+    });
+    $('#sidebarCollapse').on('click', function() {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});
+
+function myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = window.document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = window.document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[2];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
