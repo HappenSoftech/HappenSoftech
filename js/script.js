640de00f50b0fd9dd59a4244ac54cfb2df1feb6c
swagking0 <bhargav.mohith@gmail.com>
2019-05-20 04:22:37 +0200

    transport-add

--- js/script.js
+++ js/script.js
@@ -37,3 +37,22 @@ function myFunction() {
         }
     }
 }
+
+function Bus_myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = window.document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = window.document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
